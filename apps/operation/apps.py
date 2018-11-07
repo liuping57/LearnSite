4c5ae0de19e482fd09ed488a95a45fe30486feca
xinjie <2314388662@qq.com>
2018-11-07 12:37:03 +0800

    配置了页头页脚信息，修改了菜单的顺序，配置apps中文名

--- apps/operation/apps.py
+++ apps/operation/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class OperationConfig(AppConfig):
     name = 'operation'
+    verbose_name = u"用户操作"
+
+
+
--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -2,28 +2,60 @@
 # @Time    : 2018/11/7 11:13
 # @Author  : xinjie
 
-import xadmin
+# encoding: utf-8
+from courses.models import Course, Video, Lesson, CourseResource
 
-from .models import Userprofile, EmailVerifyRecord, Banner
+import xadmin
+from django.contrib.auth.models import Group, Permission
+
+from operation.models import CourseComments, UserFavorite, UserMessage, UserCourse, UserAsk
+from organization.models import CityDict, Teacher, CourseOrg
+from xadmin.models import Log
+
+# 和X admin的view绑定
+from xadmin import views
+
+from .models import EmailVerifyRecord, Banner, Userprofile
+
+
+# X admin的全局配置信息设置
+class BaseSetting(object):
+    # 主题功能开启
+    enable_themes = True
+    use_bootswatch = True
+
+
+# x admin 全局配置参数信息设置
+class GlobalSettings(object):
+    site_title = "天涯明月笙: 慕课后台管理站"
+    site_footer = "mtianyan's mooc"
+    # 收起菜单
+    menu_style = "accordion"
 
 
 # 创建admin的管理类,这里不再是继承admin，而是继承object
 class EmailVerifyRecordAdmin(object):
     # 配置后台我们需要显示的列
-    list_display = ['code', 'email', 'send_type', 'send_time']
+    list_display = ['code', 'email','send_type', 'send_time']
     # 配置搜索字段,不做时间搜索
-    search_fields =  ['code', 'email','send_type']
+    search_fields = ['code', 'email', 'send_type']
     # 配置筛选字段
-    list_filter =  ['code', 'email','send_type', 'send_time']
+    list_filter = ['code', 'email', 'send_type', 'send_time']
 
 
 # 创建banner的管理类
 class BannerAdmin(object):
     list_display = ['title', 'image', 'url','index', 'add_time']
     search_fields = ['title', 'image', 'url','index']
-    list_filter = ['title', 'image', 'url', 'index', 'add_time']
+    list_filter = ['title', 'image', 'url','index', 'add_time']
 
 
 # 将model与admin管理器进行关联注册
-xadmin.site.register(Banner, BannerAdmin)
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
+xadmin.site.register(Banner, BannerAdmin)
+
+# 将Xadmin全局管理器与我们的view绑定注册。
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+
+# 将头部与脚部信息进行注册:
+xadmin.site.register(views.CommAdminView, GlobalSettings)
